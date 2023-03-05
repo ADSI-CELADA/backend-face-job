76399cd5dd9b962e576829e7afe35de43b9ca4f8
kebyn10 <kebinochoa10@gmail.com>
2023-03-05 15:55:12 +0100

    probando mi rama

--- router/profileUser.routing.js
+++ router/profileUser.routing.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { sendMail, validateCode, updatePassword } from "../controllers/gestiónUsuarios.js";
+import { sendMail, validateCode, updatePassword,dataUser } from "../controllers/gestiónUsuarios.js";
 
 export const profileUser = Router()
 
 profileUser.post("/senMail", sendMail)
 profileUser.post("/recovery", validateCode)
 profileUser.put("/updatePass/:email", updatePassword)
+profileUser.get('/dataUser',dataUser)
