296a04998d8099871d5c62e080b1b0749c61e317
kebyn10 <kebinochoa10@gmail.com>
2023-04-17 01:12:42 -0500

    modulo de proteccion de rutas terminado

--- router/Admin.Routes.js
+++ router/Admin.Routes.js
@@ -1,4 +1,4 @@
-import { reports,solucion,messagesUsers } from "../controllers/admin.js";
+import { reports,solucion,messagesUsers,deleteAccountAdmin } from "../controllers/admin.js";
 import { Router } from "express";
 
 export const RouterAdmin=Router()
@@ -6,3 +6,4 @@ export const RouterAdmin=Router()
 RouterAdmin.get('/reports',reports)
 RouterAdmin.post('/solucion',solucion)
 RouterAdmin.post('/messagesUsers',messagesUsers)
+RouterAdmin.post('/deleteAccountAdmin',deleteAccountAdmin)
